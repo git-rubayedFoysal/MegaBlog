3d988189d6b0b08e137a03baa130ecde4381791c
Rubayed Ahmed Foysal <rubayedahmedfoysal@gmail.com>
2026-06-15 16:53:55 +0600

    deploy project

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ function Button({
   return (
     <button
       type={type}
-      className={`py-2.5 px-6 rounded-lg font-medium transition-all duration-200 hover:shadow-lg hover:scale-105 active:scale-95 ${className} ${bgColor} ${textColor}`}
+      className={`py-2.5 px-6 rounded-lg cursor-pointer font-medium transition-all duration-200 hover:shadow-lg hover:scale-105 active:scale-95 ${className} ${bgColor} ${textColor}`}
       {...props}
     >
       {children}
